d9dd1c8734f213bd562c44895ec74d3a71115eeb
dclnbrght <declanbright@gmail.com>
2025-07-01 21:09:59 +0100

    remove fretMonkey app

--- app/fretmonkey/index.js
+++
@@ -1,4 +0,0 @@
-import './components/header-bar/header-bar.js';
-import './components/fret-board/fret-board.js';
-import './components/note-marker/note-marker.js';
-import './components/filter-bar/filter-bar.js';
--- app/fretmonkey/settings.js
+++
@@ -1,8 +0,0 @@
-// Default settings for filter bar controls
-export const defaultSettings = {
-  key: 'C',
-  scale: 'major',
-  mode: 'ionian',
-  position: 'open',
-  showAllNotes: true
-};
