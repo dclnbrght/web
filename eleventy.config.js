47beb37c6d94da138400bc3bfea26b02598312d5
dclnbrght <declanbright@gmail.com>
2026-01-03 21:41:19 +0000

    feed alternate link tags

--- eleventy.config.js
+++ eleventy.config.js
@@ -19,8 +19,26 @@ export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("styles/2025.2.css");
 
 	eleventyConfig.addPlugin(feedPlugin, {
-		type: "atom", 			// or "rss", "json"
-		outputPath: "/feed.xml",
+		type: "atom", 			// "atom", "rss", "json"
+		outputPath: "/feed.atom.xml",
+		collection: {
+			name: "article", 	// iterate over `collections.article`, from "tags: article" in front matter
+			limit: 10,     		// 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "Declan Bright",
+			subtitle: "",
+			base: "https://declanbright.com/",
+			author: {
+				name: "Declan Bright"
+			}
+		}
+	});
+
+	eleventyConfig.addPlugin(feedPlugin, {
+		type: "json", 			// "atom", "rss", "json"
+		outputPath: "/feed.json",
 		collection: {
 			name: "article", 	// iterate over `collections.article`, from "tags: article" in front matter
 			limit: 10,     		// 0 means no limit
